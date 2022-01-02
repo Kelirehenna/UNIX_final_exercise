cd61b5a8383d286bde2782f27486cdde6b5d332e
Sarka <s.janakova2@gmail.com>
2022-01-02 21:51:31 +0100

    solution to final exercise

---
+++ plots.R
@@ -0,0 +1,85 @@
+setwd('~/projects/final-exercise')
+
+library(tidyverse)
+
+
+#plot showing the quality distribution by position on chromosome
+read_tsv('data/data-whole-g.tsv') -> a
+
+ggplot(a,aes(POS, QUAL)) + geom_point(size=0.1, color="plum4") +
+  ggtitle("PHRED quality distribution in genome") +
+  ylab("quality") +
+  xlab("position") -> pa
+pa
+
+#plots showing the quality by the chromosome
+#chromosome 1-6
+read_tsv('data/data-chr1-6.tsv', col_names = c("CHROM", "POS", "QUAL")) -> b
+ggplot(b,aes(POS, QUAL)) + geom_point(size = 0.1, color="steelblue3") + 
+  facet_wrap(~CHROM, ncol = 2) +
+  ggtitle("PHRED quality distribution in chromosome 1 - 6") +
+  ylab("quality") +
+  xlab("position") -> pb
+pb
+
+#chromosome 7-12
+read_tsv('data/data-chr7-12.tsv', col_names = c("CHROM", "POS", "QUAL")) -> c
+ggplot(c,aes(POS, QUAL)) + geom_point(size = 0.1, color="deepskyblue3") + 
+  facet_wrap(~CHROM, ncol = 2) +
+  ggtitle("PHRED quality distribution in chromosome 7 - 12") +
+  ylab("quality") +
+  xlab("position") -> pc
+pc
+
+#chromosome 13-18
+read_tsv('data/data-chr13-18.tsv', col_names = c("CHROM", "POS", "QUAL")) -> d
+ggplot(d,aes(POS, QUAL)) + geom_point(size = 0.1, color="cornflowerblue") + 
+  facet_wrap(~CHROM, ncol = 2) +
+  ggtitle("PHRED quality distribution in chromosome 13 - 18") +
+  ylab("quality") +
+  xlab("position") -> pd
+pd
+
+#chromosome 19-24
+read_tsv('data/data-chr19-24.tsv', col_names = c("CHROM", "POS", "QUAL")) -> e
+ggplot(e, aes(POS, QUAL)) + geom_point(size = 0.1, color="royalblue") + 
+  facet_wrap(~CHROM, ncol = 2) +
+  ggtitle("PHRED quality distribution in chromosome 19 - 24") +
+  ylab("quality") +
+  xlab("position") -> pe
+pe
+
+#chromosome 25-Z 
+read_tsv('data/data-chr25-Z.tsv', col_names = c("CHROM", "POS", "QUAL")) -> f
+ggplot(f, aes(POS, QUAL)) + geom_point(size = 0.1, color="lightseagreen") + 
+  facet_wrap(~CHROM, ncol = 1) +
+  ggtitle("PHRED quality distribution in chromosome 25 - Z") +
+  ylab("quality") +
+  xlab("position") -> pf
+pf
+
+
+#saving plots in the results folder
+jpeg("results/whole-genome.jpg",width=1200*0.8,height=800*0.8, quality = 100)
+plot(pa)
+dev.off()
+
+jpeg("results/chr1-6.jpg",width=1200*0.8,height=800*0.8, quality = 100)
+plot(pb)
+dev.off()
+
+jpeg("results/chr7-12.jpg",width=1200*0.8,height=800*0.8, quality = 100)
+plot(pc)
+dev.off()
+
+jpeg("results/chr13-18.jpg",width=1200*0.8,height=800*0.8, quality = 100)
+plot(pd)
+dev.off()
+
+jpeg("results/chr19-24.jpg",width=1200*0.8,height=800*0.8, quality = 100)
+plot(pe)
+dev.off()
+
+jpeg("results/chr25-Z.jpg",width=1200*0.8,height=800*0.8, quality = 100)
+plot(pf)
+dev.off()
---
+++ workflow.sh
@@ -0,0 +1,40 @@
+< $GENOM grep -e '^chr[1]\s' -e '^chr[2]\s'  > ./data/data-chr1-10.tsv
+#!/bin/bash
+
+echo "Preparing directories and data"
+
+#preparation of your working directory and data
+mkdir data
+mkdir results
+cp /data-shared/vcf_examples/luscinia_vars.vcf.gz data 
+
+#make a variable for the input data
+
+echo "Preparing data for genome analysis"
+
+INPUTDATA=./data/luscinia_vars.vcf.gz
+
+#preparation of the file for the whole genome quality analysis
+#remove header part from data
+#extract desired columns - chromosome number (CHROM), the position (POS) and the quality score (QUAL), put into new file
+#remove the first # from the name line
+zcat $INPUTDATA | grep -v '^##' | cut -f1,2,6 | tail -c +2 > ./data/data-whole-g.tsv
+
+
+#preparation of the files for the by chromosome analysis
+
+echo "Preparing data for chromosone analysis"
+
+GENOM=./data/data-whole-g.tsv
+
+#making the files for the by chromosome graphs
+< $GENOM grep -e '^chr1\s' -e '^chr2\s' -e '^chr3\s' -e '^chr4\s' -e '^chr5\s' -e '^chr6\s' > ./data/data-chr1-6.tsv
+< $GENOM grep -e '^chr7\s' -e '^chr8\s' -e '^chr9\s' -e '^chr10\s' -e '^chr11\s' -e '^chr12\s' > ./data/data-chr7-12.tsv
+< $GENOM grep -e '^chr13\s' -e '^chr14\s' -e '^chr15\s' -e '^chr16\s' -e '^chr17\s' -e '^chr18\s' > ./data/data-chr13-18.tsv
+< $GENOM grep -e '^chr19\s' -e '^chr20\s' -e '^chr21\s' -e '^chr22\s' -e '^chr23\s' -e '^chr24\s' > ./data/data-chr19-24.tsv
+< $GENOM grep -e '^chr25\s' -e '^chr26\s' -e '^chr27\s' -e '^chrZ\s' > ./data/data-chr25-Z.tsv
+
+
+#run the rscript for the graphs
+Rscript plots.R
+
